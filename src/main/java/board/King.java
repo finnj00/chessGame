49f9f42a8eee79c93f953aba44da78dce94c3585
Finnigan Johnson <56231926+finnj00@users.noreply.github.com>
2021-02-06 16:16:09 -0800

    Added getMove methods for king and knight

--- src/main/java/board/King.java
+++ src/main/java/board/King.java
@@ -1,7 +1,33 @@
 package board;
 
+import game.ChessMove;
+
 public class King extends ChessPiece{
     public King(int x, int y, boolean team) {
         super(x, y, team);
+    }
+
+    @Override
+    public void getMoves() {
+        if(y < 7) {
+            this.moves.add(new ChessMove(x, y + 1));
+            if(x > 0) {
+                this.moves.add(new ChessMove(x - 1, y + 1));
+                this.moves.add(new ChessMove(x - 1, y));
+            }
+            if(x < 7) {
+                this.moves.add(new ChessMove(x + 1, y + 1));
+                this.moves.add(new ChessMove(x + 1, y));
+            }
+        }
+        if(y > 0) {
+            this.moves.add(new ChessMove(x, y - 1));
+            if(x > 0) {
+                this.moves.add(new ChessMove(x - 1, y - 1));
+            }
+            if(x < 7) {
+                this.moves.add(new ChessMove(x + 1, y - 1));
+            }
+        }
     }
 }
--- src/main/java/board/Knight.java
+++ src/main/java/board/Knight.java
@@ -1,7 +1,52 @@
 package board;
 
+import game.ChessMove;
+
 public class Knight extends ChessPiece{
     public Knight(int x, int y, boolean team) {
         super(x, y, team);
+    }
+
+    @Override
+    public void getMoves() {
+        // Top two moves
+        if(y < 6) {
+            if(x > 0) {
+                this.moves.add(new ChessMove(x - 1, y + 2));
+            }
+            if(x < 7) {
+                this.moves.add(new ChessMove(x + 1, y + 2));
+            }
+        }
+
+        // Two right moves
+        if(x < 6) {
+            if(y > 0) {
+                this.moves.add(new ChessMove(x + 2, y - 1));
+            }
+            if(y < 7) {
+                this.moves.add(new ChessMove(x + 2, y + 1));
+            }
+        }
+
+        // Two left moves
+        if(x > 1) {
+            if(y > 0) {
+                this.moves.add(new ChessMove(x - 2, y - 1));
+            }
+            if(y < 7) {
+                this.moves.add(new ChessMove(x - 2, y + 1));
+            }
+        }
+
+        // Two bottom moves
+        if(y > 1) {
+            if(x > 0) {
+                this.moves.add(new ChessMove(x - 1, y - 2));
+            }
+            if(x < 7) {
+                this.moves.add(new ChessMove(x + 1, y - 2));
+            }
+        }
     }
 }
